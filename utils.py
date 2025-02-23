ff8e3ad35e99790c86b8722c8c479ab0a246bde8
top34051 <top34051@gmail.com>
2025-02-23 15:04:19 -0800

    support multiple responses

--- utils.py
+++ utils.py
@@ -35,38 +35,49 @@ client = MongoClient(st.secrets["MONGO_URI"])
 db = client["emoai"]
 
 
-def save_response(participant_id, scenario_name, response=None):
+def save_response(participant_id, scenario_name, response_index, response=None):
     collection = db["responses"]
 
     if response is None:
         if not st.session_state.get(
-            f"reply_text_area_{participant_id}_{scenario_name}", None
+            f"reply_text_area_{participant_id}_{scenario_name}_{response_index}", None
         ):
             return
-        response = st.session_state[f"reply_text_area_{participant_id}_{scenario_name}"]
+        response = st.session_state[
+            f"reply_text_area_{participant_id}_{scenario_name}_{response_index}"
+        ]
 
     collection.update_one(
-        {"participant_id": participant_id, "scenario_name": scenario_name},
+        {
+            "participant_id": participant_id,
+            "scenario_name": scenario_name,
+            "response_index": response_index,
+        },
         {"$set": {"response": response}},
         upsert=True,
     )
 
 
-def get_saved_response(participant_id, scenario_name):
+def get_saved_response(participant_id, scenario_name, response_index):
     collection = db["responses"]
     result = collection.find_one(
-        {"participant_id": participant_id, "scenario_name": scenario_name}
+        {
+            "participant_id": participant_id,
+            "scenario_name": scenario_name,
+            "response_index": response_index,
+        }
     )
     return result["response"] if result else None
 
 
-def send_response(participant_id, scenario_name):
+def send_response(participant_id, scenario_name, response_index):
     collection = db["sent_responses"]
-    response = get_saved_response(participant_id, scenario_name)
+    response = get_saved_response(participant_id, scenario_name, response_index)
     collection.update_one(
         {
             "participant_id": participant_id,
             "scenario_name": scenario_name,
+            "response_index": response_index,
         },
         {
             "$set": {
@@ -78,10 +89,14 @@ def send_response(participant_id, scenario_name):
     )
 
 
-def get_sent_response(participant_id, scenario_name):
+def get_sent_response(participant_id, scenario_name, response_index):
     collection = db["sent_responses"]
     result = collection.find_one(
-        {"participant_id": participant_id, "scenario_name": scenario_name}
+        {
+            "participant_id": participant_id,
+            "scenario_name": scenario_name,
+            "response_index": response_index,
+        }
     )
     return result["response"] if result else None
 
@@ -113,40 +128,49 @@ def scenario(
         f"<div class='email-content'>{email_content}</div>",
         unsafe_allow_html=True,
     )
-    if (sent_response := get_sent_response(participant_id, scenario_name)) is not None:
-        icon, content = st.columns([1, 14])
-        icon.image("images/icon.png")
-        content.markdown(
-            "<div class='email-sender'>You <span class='email-sender-email' style='margin-left: 2px;'>you@emoai.com</span></div>",
-            unsafe_allow_html=True,
-        )
-        content.markdown(
-            f"<div class='email-content'>{sent_response}</div>",
-            unsafe_allow_html=True,
-        )
-    elif get_saved_response(participant_id, scenario_name) is not None:
-        icon, content = st.columns([1, 14])
-        icon.image("images/icon.png")
 
-        response = get_saved_response(participant_id, scenario_name)
-        content.text_area(
-            "Reply",
-            height=250,
-            value=response,
-            label_visibility="hidden",
-            key=f"reply_text_area_{participant_id}_{scenario_name}",
-            on_change=save_response(participant_id, scenario_name),
-        )
-        if content.button(
-            "Send",
-            key="send_button",
-        ):
-            send_response(participant_id, scenario_name)
-            st.rerun()
-    else:
-        if content.button(
-            "Reply",
-            key="reply_button",
-        ):
-            save_response(participant_id, scenario_name, signature)
-            st.rerun()
+    i = 0
+    while True:
+        if (
+            sent_response := get_sent_response(participant_id, scenario_name, i)
+        ) is not None:
+            icon, content = st.columns([1, 14])
+            icon.image("images/icon.png")
+            content.markdown(
+                "<div class='email-sender'>You <span class='email-sender-email' style='margin-left: 2px;'>you@emoai.com</span></div>",
+                unsafe_allow_html=True,
+            )
+            content.markdown(
+                f"<div class='email-content'>{sent_response}</div>",
+                unsafe_allow_html=True,
+            )
+        elif (
+            response := get_saved_response(participant_id, scenario_name, i)
+        ) is not None:
+            icon, content = st.columns([1, 14])
+            icon.image("images/icon.png")
+            content.text_area(
+                "Reply",
+                height=250,
+                value=response,
+                label_visibility="hidden",
+                key=f"reply_text_area_{participant_id}_{scenario_name}_{i}",
+                on_change=save_response(participant_id, scenario_name, i),
+            )
+            if content.button(
+                "Send",
+                key="send_button",
+            ):
+                send_response(participant_id, scenario_name, i)
+                save_response(participant_id, scenario_name, i, signature)
+                st.rerun()
+            break
+        else:
+            if content.button(
+                "Reply",
+                key="reply_button",
+            ):
+                save_response(participant_id, scenario_name, i, signature)
+                st.rerun()
+            break
+        i += 1
